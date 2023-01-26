5108cfb2ed55ce03f32f9c191c1b2c75bbdbda62
touhidul hasan <hasanmd.touhidul05@gmail.com>
2023-01-26 22:18:50 +0600

    new line

--- index.php
+++ index.php
@@ -7,3 +7,26 @@ $n2 = 200;
 $n3 = 300;
 
 echo $n2;
+echo "\n";
+
+// ternary operator
+
+$num1 = 88;
+$num2 = 101;
+$num3 = 203;
+
+if($num1 > $num2 && $num1 > $num3){
+    echo "$num1 is the greatest number";
+}elseif($num2 > $num1 && $num2 > $num3){
+    echo "$num2 is greater number";
+}elseif($num3 > $num2 && $num3 > $num1){
+    echo "$num3 is greater number";
+}else{
+    echo "this is a number";
+}
+
+echo "\n";
+// ternary operator
+
+$number = ($num1 > $num2 && $num1 > $num3) ? "large number $num1" : (($num2 > $num1 && $num2 > $num3) ? "large number $num2" : (($num3 > $num2 && $num3 > $num1) ? "large number $num3" : "equal"));
+echo $number;
